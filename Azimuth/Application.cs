7b5e5c4fcb3dab5c59fcd84e20c5f9b0e96af173
Konrad-Haddadi <124107893+Konrad-Haddadi@users.noreply.github.com>
2023-02-24 12:14:16 +1100

    framework update

---
+++ Azimuth/Application.cs
@@ -0,0 +1,50 @@
+﻿using Azimuth.GameObject;
+
+using Raylib_cs;
+
+namespace Azimuth
+{
+	public sealed class Application
+	{
+		public static Application Instance { get; private set; }
+
+		public static void Run(int _width, int _height, string _title, Color _color, Game _game)
+		{
+			Instance = new Application(_width, _height, _title, _color, _game);
+			Instance.Run();
+		}
+		public Window Window { get; }
+
+		private readonly Game game;
+
+		public Application(int _width, int _height, string _name, Color _color, Game _game)
+		{
+			Window = new Window(_width, _height, _name, _color);
+			
+			game = _game;
+		}
+
+		private void Run()
+		{
+			Window.Open();
+			game.Load();
+
+			while(!Raylib.WindowShouldClose())
+			{
+				float dealtaTime = Raylib.GetFrameTime();
+				GameObjectManager.Update(dealtaTime);
+				
+				Raylib.BeginDrawing();
+				Window.Clear();
+				
+				// Global drawing here
+				GameObjectManager.Draw();
+				
+				Raylib.EndDrawing();
+			}
+			
+			game.Unload();
+			Window.Close();
+		}
+	}
+}
